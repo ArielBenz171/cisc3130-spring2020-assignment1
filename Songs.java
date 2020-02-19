3263f108d0833653413dd501818efe8201173fbb
ArielBenz171 <61249467+ArielBenz171@users.noreply.github.com>
2020-02-19 14:37:47 -0500

    Update Songs.java

--- Songs.java
+++ Songs.java
@@ -2,11 +2,17 @@ import java.io.*;
 import java.util.*;
 class Songs{
   public static void main(String[] args){ throws IOException
-    Scanner sc = new Scanner(new File(" 
+    Scanner sc = new Scanner(new File("regional-global-daily-latest.csv"));
     int cols = 4;
     int rows = 199;
     String[][] myList = new String[rows][cols];
-    for(int i = 0; i < cols; i++){
-      
+    for(int i = 0; i < rows; i++){
+      for(int j = 0; j < cols; j++){
+        myList[i][j] = sc.hasNext();
+      }
+    }
     int[][] arr = { { 1, 2 }, { 3, 4 } };
     System.out.println("arr[0][0] = " + arr[0][0]);
+    sc.close();
+  }
+}

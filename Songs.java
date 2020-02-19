3a62dff397e856a8bbb3d427627a2fc1b09ccb17
ArielBenz171 <61249467+ArielBenz171@users.noreply.github.com>
2020-02-19 14:33:23 -0500

    Create Songs.java

---
+++ Songs.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Songs{
+  public static void main(String[] args){ throws IOException
+    Scanner sc = new Scanner(new File(" 
+    int cols = 4;
+    int rows = 199;
+    String[][] myList = new String[rows][cols];
+    for(int i = 0; i < cols; i++){
+      
+    int[][] arr = { { 1, 2 }, { 3, 4 } };
+    System.out.println("arr[0][0] = " + arr[0][0]);
